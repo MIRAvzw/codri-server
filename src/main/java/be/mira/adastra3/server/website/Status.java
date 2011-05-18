feda8670720df1fc782ffd05c1908b1a8100fbd3
Tim Besard <tim.besard@gmail.com>
2011-05-18 11:18:12 +0200

    Switching to JWt.

---
+++ src/main/java/be/mira/adastra3/server/website/Status.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.mira.adastra3.server.website;
+
+import be.mira.adastra3.server.website.status.StatusApplication;
+import eu.webtoolkit.jwt.WApplication;
+import eu.webtoolkit.jwt.WEnvironment;
+import eu.webtoolkit.jwt.WtServlet;
+
+public class Status extends WtServlet {
+    private static final long serialVersionUID = 1L;
+
+    public Status() {
+        super();
+    }
+
+    @Override
+    public WApplication createApplication(WEnvironment env) {
+        /*
+         * You could read information from the environment to decide whether the
+         * user has permission to start a new application
+         */
+        return new StatusApplication(env);
+    }
+}
+
--- src/main/java/be/mira/adastra3/server/website/status/StatusApplication.java
+++ src/main/java/be/mira/adastra3/server/website/status/StatusApplication.java
@@ -1,188 +1,40 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package be.mira.adastra3.server.website.status;
 
-import be.mira.adastra3.server.website.status.data.MachineContainer;
-import be.mira.adastra3.server.website.status.ui.HelpWindow;
-import be.mira.adastra3.server.website.status.ui.MachineDetail;
-import be.mira.adastra3.server.website.status.ui.MachineList;
-import be.mira.adastra3.server.website.status.ui.MachineView;
-import be.mira.adastra3.server.website.status.ui.NavigationTree;
-import com.vaadin.Application;
-import com.vaadin.data.Item;
-import com.vaadin.data.Property;
-import com.vaadin.data.Property.ValueChangeEvent;
-import com.vaadin.data.Property.ValueChangeListener;
-import com.vaadin.event.ItemClickEvent;
-import com.vaadin.event.ItemClickEvent.ItemClickListener;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.SplitPanel;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Window;
+import eu.webtoolkit.jwt.Side;
+import eu.webtoolkit.jwt.Signal;
+import eu.webtoolkit.jwt.WApplication;
+import eu.webtoolkit.jwt.WBreak;
+import eu.webtoolkit.jwt.WEnvironment;
+import eu.webtoolkit.jwt.WLineEdit;
+import eu.webtoolkit.jwt.WPushButton;
+import eu.webtoolkit.jwt.WText;
 
-/**
- *
- * @author tim
+/*
+ * A simple hello world application class which demonstrates how to react
+ * to events, read input, and give feed-back.
  */
-public class StatusApplication extends Application implements ItemClickListener, Button.ClickListener, ValueChangeListener {
-    //
-    // Member data
-    //
-
-    private NavigationTree tree = new NavigationTree(this);
-    private SplitPanel horizontalSplit = new SplitPanel(SplitPanel.ORIENTATION_HORIZONTAL);
-    private Button mButtonRefresh = new Button("Refresh");
-    private Button mButtonHelp = new Button("Help");
-
-    // Lazyly created ui references
-    private MachineView mMachineView = null;
-    private MachineList mMachineList = null;
-    private MachineDetail mMachineDetail = null;
-    private HelpWindow mHelpWindow = null;
-    private MachineContainer mDataSource;
-
-
-    //
-    // UI Initialisation
-    //
-
-    @Override
-    public void init() {
-        getDataSource();
-        buildMainLayout();
-        setMainComponent(getListView());
-    }
-
-    private void buildMainLayout() {
-        setMainWindow(new Window("MIRA Ad-Astra III status application"));
-
-        VerticalLayout layout = new VerticalLayout();
-        layout.setSizeFull();
-
-        layout.addComponent(createToolbar());
-        layout.addComponent(horizontalSplit);
-        layout.setExpandRatio(horizontalSplit, 1);
-
-        horizontalSplit.setSplitPosition(200, SplitPanel.UNITS_PIXELS);
-        horizontalSplit.setFirstComponent(tree);
-
-        getMainWindow().setContent(layout);
-    }
-
-    private HorizontalLayout createToolbar() {
-        HorizontalLayout lo = new HorizontalLayout();
-        lo.addComponent(mButtonRefresh);
-        lo.addComponent(mButtonHelp);
-
-        mButtonRefresh.addListener((ClickListener) this);
-        mButtonHelp.addListener((ClickListener) this);
+public class StatusApplication extends WApplication {
+    public StatusApplication(WEnvironment env) {
+        super(env);
         
-        lo.setStyleName("toolbar");
+        setTitle("Hello world");
 
-        return lo;
-    }
+        getRoot().addWidget(new WText("Your name, please ? "));
+        final WLineEdit nameEdit = new WLineEdit(getRoot());
+        nameEdit.setFocus();
 
-    //
-    // Event handlers
-    //
+        WPushButton button = new WPushButton("Greet me.", getRoot());
+        button.setMargin(5, Side.Left);
 
-    public void buttonClick(Button.ClickEvent event) {
-        final Object source = event.getSource();
+        getRoot().addWidget(new WBreak());
 
-        if (source instanceof Button) {
-            if (source == mButtonHelp) {
-                showHelpWindow();
-            } else if (source == mButtonRefresh) {
-                refreshDataSource();
-                // Todo: refresh list view etc
+        final WText greeting = new WText(getRoot());
+
+        button.clicked().addListener(this, new Signal.Listener() {
+            @Override
+            public void trigger() {
+                greeting.setText("Hello there, " + nameEdit.getText());
             }
-        }
-    }
-
-    public void itemClick(ItemClickEvent event) {
-        if (event.getSource() == tree) {
-            Object itemId = event.getItemId();
-            if (itemId != null) {
-                if (NavigationTree.SHOW_ALL.equals(itemId)) {
-                    getDataSource().removeAllContainerFilters();
-                } else if (NavigationTree.SERVERS.equals(itemId)) {
-                    getDataSource().removeAllContainerFilters();
-                    getDataSource().addContainerFilter("type", "Server", false, false);
-                    // Eventueel ook subfilters (offline, online)
-                } else if (NavigationTree.KIOSKS.equals(itemId)) {
-                    getDataSource().removeAllContainerFilters();
-                    getDataSource().addContainerFilter("type", "Kiosk", false, false);
-                    // Eventueel ook subfilters (offline, online)
-                }
-                showListView();
-            }
-        }
-    }
-
-    public void valueChange(ValueChangeEvent event) {
-        Property property = event.getProperty();
-        if (property == mMachineList) {
-            Item item = mMachineList.getItem(mMachineList.getValue());
-            if (item != mMachineDetail.getItemDataSource()) {
-                mMachineDetail.setItemDataSource(item);
-            }
-        }
-    }
-
-
-    //
-    // Data handling
-    //
-
-    public MachineContainer getDataSource() {
-        if (mDataSource == null)
-            mDataSource = MachineContainer.createFromTopology();
-        return mDataSource;
-    }
-
-    public void refreshDataSource() {
-        getDataSource().updateFromTopology();
-    }
-
-
-    //
-    // Auxiliary
-    //
-
-    private void setMainComponent(Component c) {
-        horizontalSplit.setSecondComponent(c);
-    }
-
-    /*
-     * View getters exist so we can lazily generate the views, resulting in
-     * faster application startup time.
-     */
-    private MachineView getListView() {
-        if (mMachineView == null) {
-            mMachineList = new MachineList(this);
-            mMachineDetail = new MachineDetail();
-            mMachineView = new MachineView(mMachineList, mMachineDetail);
-        }
-        return mMachineView;
-    }
-
-    private HelpWindow getHelpWindow() {
-        if (mHelpWindow == null) {
-            mHelpWindow = new HelpWindow();
-        }
-        return mHelpWindow;
-    }
-
-    private void showHelpWindow() {
-        getMainWindow().addWindow(getHelpWindow());
-    }
-
-    private void showListView() {
-        setMainComponent(getListView());
+        });
     }
 }
